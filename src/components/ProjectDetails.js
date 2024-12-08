758a0b96b0991b1ca16d34e400af53d3a13a469e
Faiz <faizhadiyanfirza@gmail.com>
2024-12-08 15:49:59 +0700

    revisi logo

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -39,7 +39,7 @@ export default function ProjectDetails({ title, description, image, appStoreLink
               <a href={appStoreLink} target="_blank" rel="noopener noreferrer" className="inline-flex items-center px-4 py-2 bg-[#0071E3] text-white rounded-full hover:bg-[#0077ED] transition-colors text-sm sm:text-base">
                 <span className="mr-2">View in App Store</span>
                 <svg className="w-4 h-4 sm:w-5 sm:h-5" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M12 2C6.477 2 2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.879V14.89h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.989C18.343 21.129 22 16.99 22 12c0-5.523-4.477-10-10-10z" />
+                  <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11" />
                 </svg>
               </a>
             )}
